04801048144e400d49647b8d1b859532aa4b0306
fernzaaron <aaron.fernandez@ontariotechu.net>
2025-11-04 21:40:57 -0500

    portfolio img addition, adding code from a1

--- MyPortfolio/routes/index.js
+++ MyPortfolio/routes/index.js
@@ -3,37 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Home' 
-  });
+  res.render('home', { title: 'Home' });
 });
 
-/* GET home page. */
+/* Optional: GET /home as alias */
 router.get('/home', function(req, res, next) {
-  res.render('index', { 
-    title: 'Home' 
-  });
+  res.render('home', { title: 'Home' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { 
-    title: 'About' 
-  });
+  res.render('about', { title: 'About' });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { 
-    title: 'Projects' 
-  });
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { 
-    title: 'Contact' 
-  });
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
